2dec481ecd21a6cf6045098d9a487276d2466ea1
rrtigga <rrtigga@ucdavis.edu>
2016-11-12 21:22:32 -0800

    added all files

---
+++ public/javascripts/posts.js
@@ -0,0 +1,431 @@
+var items = [
+   {
+      "id": "0",
+      "description": "Mexico",
+      "imageURL":"https://pbs.twimg.com/media/CxE1fi8XEAEATGo.jpg"
+   },
+   {
+      "id": "1",
+      "description": "University of Michigan Hijab",
+      "imageURL":"https://pbs.twimg.com/media/CxFsnrlUcAA6t1R.jpg:large"
+   },
+   {
+      "id": "2",
+      "description": "Reno slave threat",
+      "imageURL":"https://pbs.twimg.com/media/CxFTh9cVEAE1Wqq.jpg:large"
+   },
+   {
+      "id": "3",
+      "description": "Go back to Mexico",
+      "imageURL":"https://pbs.twimg.com/media/CxFH9iqUQAAg0hE.jpg:large"
+   },
+   {
+      "id": "4",
+      "description": "Sexually harassed",
+      "imageURL":"https://pbs.twimg.com/media/CxFG_rbUoAAkBaf.jpg:large"
+   },
+   {
+      "id": "5",
+      "description": "swastika",
+      "imageURL":"https://pbs.twimg.com/media/CxEvXy0XgAECkOS.jpg:large"
+   },
+   {
+      "id": "6",
+      "description": "Walking and almost ran over",
+      "imageURL":"https://pbs.twimg.com/media/CxFDQNfUAAACeBx.jpg:large"
+   },
+   {
+      "id": "7",
+      "description": "Swastika",
+      "imageURL":"https://pbs.twimg.com/media/CxFC4gSUAAAKhwr.jpg:large"
+   },
+   {
+      "id": "8",
+      "description": "Latinos for H",
+      "imageURL":"https://pbs.twimg.com/media/CxE033qXEAANSwg.jpg:large"
+   },
+   {
+      "id": "9",
+      "description": "Seattle Gay Hate Crime",
+      "imageURL":"https://pbs.twimg.com/media/CxEoJPBXcAEd8ov.jpg"
+   },
+   {
+      "id": "10",
+      "description": "Whites only",
+      "productLink": "http://abc7chicago.com/news/racist-message-found-in-suburban-high-school-bathroom/1600995/",
+      "imageURL":"https://o.twimg.com/2/proxy.jpg?t=HBhYaHR0cDovL2Nkbi5hYmNsb2NhbC5nby5jb20vY29udGVudC93bHMvaW1hZ2VzL2Ntcy9hdXRvbWF0aW9uL2ltYWdlcy8xNjAwOTg2XzEyODB4NzIwLmpwZxSAFBS6ChwUhAYUlAMAABYAEgA&s=1Ddvhfx1Vb7wnmpr2qMg1qVGv3aLya8f5yCUxKki2ok"
+   },
+   {
+      "id": "11",
+      "description": "Kansas hindi phd students",
+      "imageURL":"https://pbs.twimg.com/media/CxEntW8WgAAK81S.jpg:large"
+   },
+   {
+      "id": "12",
+      "description": "Korean American Assault",
+      "imageURL":"https://pbs.twimg.com/media/CxEg5aGWQAAhLiY.jpg:large"
+   },
+   {
+      "id": "13",
+      "description": "Hillary sticker harassed",
+      "imageURL":"https://pbs.twimg.com/media/CxEb0ctWQAAAvsj.jpg:large"
+   },
+   {
+      "id": "14",
+      "description": "Gym note n word",
+      "imageURL":"https://pbs.twimg.com/media/CxEa0KjWgAAbIAM.jpg:large"
+   },
+   {
+      "id": "15",
+      "description": "Chewing Tobacco spit on her",
+      "imageURL":"https://pbs.twimg.com/media/CxEY3naXcAEkK1G.jpg:large"
+   },
+   {
+      "id": "16",
+      "description": "Women's stall at SUNY",
+      "imageURL":"https://pbs.twimg.com/media/CxEXLh2XUAAznys.jpg:large"
+   },
+   {
+      "id": "17",
+      "description": "Trump kid supporters",
+      "imageURL":"https://pbs.twimg.com/media/CxAGoWgXEAEjvSK.jpg:large"
+   },
+   {
+      "id": "18",
+      "description": "Egged",
+      "imageURL":"https://pbs.twimg.com/media/CxA6dc2VQAAZY9N.jpg:large"
+   },
+   {
+      "id": "19",
+      "description": "Hospital N word",
+      "imageURL":"https://pbs.twimg.com/media/Cw_-WGtW8AAI_17.jpg:large"
+   },
+   {
+      "id": "20",
+      "description": "Penn State Lynch Threat",
+      "imageURL":"https://pbs.twimg.com/media/CxAVVhdUUAAke4h.jpg:large"
+   },
+   {
+      "id": "21",
+      "description": "Heard at WFU",
+      "imageURL":"https://pbs.twimg.com/media/Cw3qmLPUcAAZ6VF.jpg:large"
+   },
+   {
+      "id": "22",
+      "description": "Donald Trump her",
+      "imageURL":"https://pbs.twimg.com/media/Cw_V5YMXcAAyliH.jpg:large"
+   },
+   {
+      "id": "23",
+      "description": "grabbed 10 year old",
+      "imageURL":"https://pbs.twimg.com/media/Cw-xtTOWQAAzamR.jpg"
+   },
+   {
+      "id": "24",
+      "description": "Muslims threat terrorism hate crime",
+      "imageURL":"https://pbs.twimg.com/media/Cw_Ut4kWIAA_66p.jpg"
+   },
+   {
+      "id": "25",
+      "description": "separate fountains",
+      "imageURL":"https://pbs.twimg.com/media/Cw_SLBkXEAAgskR.jpg"
+   },
+   {
+      "id": "26",
+      "description": "make a wall mexican racist",
+      "imageURL":"https://pbs.twimg.com/media/Cw7CE0mWEAAMlHh.jpg"
+   },
+   {
+      "id": "27",
+      "description": "Racist remark UI",
+      "imageURL":"https://pbs.twimg.com/media/Cw-3IcRW8AAqxaJ.jpg"
+   },
+   {
+      "id": "28",
+      "description": "ASL hate crime",
+      "imageURL":"https://pbs.twimg.com/media/Cw8XNYVUcAAEL-A.jpg"
+   },
+   {
+      "id": "29",
+      "description": "Trash black hate crime",
+      "imageURL":"https://pbs.twimg.com/media/Cw7ZrZaVEAA2d3a.jpg:large"
+   },
+   {
+      "id": "30",
+      "description": "DMV cutting in line",
+      "imageURL":"https://pbs.twimg.com/media/Cw7ixWcUQAE_YJ2.jpg"
+   },
+   {
+      "id": "31",
+      "description": "DMV cutting in line",
+      "imageURL":"https://pbs.twimg.com/media/Cw7ixWcUQAE_YJ2.jpg"
+   },
+   {
+      "id": "32",
+      "description": "Townson University",
+      "imageURL":"https://pbs.twimg.com/media/Cw7TkL1XEAINbe5.jpg"
+   },
+   {
+      "id": "32",
+      "description": "This is Trump Land",
+      "imageURL":"https://pbs.twimg.com/media/Cw2t03ZXAAAqbb5.jpg"
+   },
+   {
+      "id": "32",
+      "description": "This is Trump Land",
+      "imageURL":"https://pbs.twimg.com/media/Cw2t03ZXAAAqbb5.jpg"
+   },
+   {
+      "id": "33",
+      "description": "High School issues",
+      "imageURL":"https://pbs.twimg.com/media/Cw7S2BaWEAI8BTw.jpg"
+   },
+   {
+      "id": "32",
+      "description": "This is Trump Land",
+      "imageURL":"https://pbs.twimg.com/media/Cw2t03ZXAAAqbb5.jpg"
+   },
+   {
+      "id": "33",
+      "description": "5th Graders Build a wall",
+      "imageURL":"https://pbs.twimg.com/media/Cw7RYupWIAAwMLA.jpg"
+   },
+   {
+      "id": "34",
+      "description": "Go Back to Asia",
+      "imageURL":"https://pbs.twimg.com/media/Cw7MgplWQAA-FBz.png:large"
+   },
+   {
+      "id": "35",
+      "description": "Iranian Muslim Woman",
+      "imageURL":"https://pbs.twimg.com/media/Cw7KorFXEAAZtLx.jpg"
+   },
+   {
+      "id": "37",
+      "description": "10 Feet Higher",
+      "imageURL":"https://pbs.twimg.com/media/Cw1Zw0HWIAAZzUn.jpg"
+   },
+   {
+      "id": "38",
+      "description": "Bye Bye Latinos",
+      "imageURL":"https://pbs.twimg.com/media/Cw6d7o2WQAA7SPz.jpg"
+   },
+   {
+      "id": "39",
+      "description": "Napa Asian Woman",
+      "imageURL":"https://pbs.twimg.com/media/Cw6htMyWIAAtrnC.jpg"
+   },
+   {
+      "id": "40",
+      "description": "Racist can you speak english",
+      "imageURL":"https://pbs.twimg.com/media/Cw6g2CkWgAA7vwL.jpg"
+   },
+   {
+      "id": "41",
+      "description": "Kids Celebrating Trump Victory",
+      "imageURL":"https://pbs.twimg.com/media/Cw6gRgkXEAAD6lW.jpg"
+   },
+   {
+      "id": "42",
+      "description": "Sister getting told she was getting deported",
+      "imageURL":"https://pbs.twimg.com/media/Cw1gy58UkAAhDif.jpg"
+   },
+   {
+      "id": "43",
+      "description": "Trump kid hits latino kid",
+      "imageURL":"https://pbs.twimg.com/media/Cw3lXmIXAAAgqUj.jpg"
+   },
+   {
+      "id": "44",
+      "description": "Gay Families",
+      "imageURL":"https://pbs.twimg.com/media/Cw3jNF-WIAEqCcf.jpg"
+   },
+   {
+      "id": "45",
+      "description": "Car Vandalized with racist remarks towards black people",
+      "imageURL":"https://dl2.pushbulletusercontent.com/yqhaURqYowsTRnJjvl9Vbs5WSHwyXWvh/Screen%20Shot%202016-11-12%20at%204.17.39%20PM.png"
+   },
+   {
+      "id": "46",
+      "description": "Trump supporter verbally abuse and doing donuts",
+      "imageURL":"http://65.media.tumblr.com/d6ae7b4114ecfad62d2061d6f7b3dd0e/tumblr_inline_ogherjLUze1t0jnq1_1280.png"
+   },
+   {
+      "id": "47",
+      "description": "SDSU hate crime",
+      "imageURL":"https://pbs.twimg.com/media/Cw3bmS_WgAAUEY-.jpg"
+   },
+   {
+      "id": "48",
+      "description": "Hijab hate crime walmart",
+      "imageURL":"https://pbs.twimg.com/media/Cw3TejBXUAIDwHd.jpg"
+   },
+   {
+      "id": "49",
+      "description": "N word on doors vandalism",
+      "imageURL":"https://pbs.twimg.com/media/Cw3QxiqXEAEQQxG.jpg"
+   },
+   {
+      "id": "50",
+      "description": "Graffiti ",
+      "imageURL":"https://dl2.pushbulletusercontent.com/VR7zXdgosWxNmKkiCmQSinAlShCDsQQp/Screen%20Shot%202016-11-12%20at%206.20.18%20PM.png"
+   },
+   {
+      "id": "51",
+      "description": "Called the police on racist white males",
+      "imageURL":"https://pbs.twimg.com/media/Cw3NePQXcAEa2UM.jpg"
+   },
+   {
+      "id": "52",
+      "description": "Called the police on racist white males",
+      "imageURL":"https://pbs.twimg.com/media/Cw3NePQXcAEa2UM.jpg"
+   },
+   {
+      "id": "53",
+      "description": "Atlanta Suburban Racism",
+      "imageURL":"https://pbs.twimg.com/media/Cw6Z2gsWEAEVTwf.jpg"
+   },
+   {
+      "id": "54",
+      "description": "Chink Racial Slur",
+      "imageURL":"https://pbs.twimg.com/media/Cw6ZQ9YXgAA24wc.jpg"
+   },
+   {
+      "id": "55",
+      "description": "N Word Safeway",
+      "imageURL":"https://pbs.twimg.com/media/Cw6TfBdWEAA7qZe.jpg"
+   },
+   {
+      "id": "56",
+      "description": "Called the police on racist white males",
+      "imageURL":"https://pbs.twimg.com/media/Cw3NePQXcAEa2UM.jpg"
+   },
+   {
+      "id": "57",
+      "description": "Go back to Africa Florida",
+      "imageURL":"https://pbs.twimg.com/media/Cw3n502UUAAfj_u.jpg"
+   },
+   {
+      "id": "58",
+      "description": "Rope to black baby",
+      "imageURL":"https://pbs.twimg.com/media/Cw2TxuSWQAAyDeJ.jpg:large"
+   },
+   {
+      "id": "59",
+      "description": "Called the police on racist white males",
+      "imageURL":"https://pbs.twimg.com/media/Cw3NePQXcAEa2UM.jpg"
+   },
+   {
+      "id": "60",
+      "description": "Make American White Again Graffiti",
+      "imageURL":"https://pbs.twimg.com/media/Cw6NJd8WEAA4eGB.jpg:large"
+   },
+   {
+      "id": "61",
+      "description": "Threat Daughter School",
+      "imageURL":"https://pbs.twimg.com/media/Cw6AlsYXUAAw_8I.jpg"
+   },
+   {
+      "id": "62",
+      "description": "Threat Daughter School",
+      "imageURL":"https://pbs.twimg.com/media/Cw6Amj5WEAAc1kG.jpg"
+   },
+   {
+      "id": "63",
+      "description": "Grabbed chest by random person on bus",
+      "imageURL":"https://pbs.twimg.com/media/Cw6AEABXEAE6tkx.jpg"
+   },
+   {
+      "id": "64",
+      "description": "Fights and racial slurs",
+      "imageURL":"https://pbs.twimg.com/media/Cw5_RpBXEAA3DE0.jpg"
+   },
+   {
+      "id": "65",
+      "description": "Back of the bus",
+      "imageURL":"https://pbs.twimg.com/media/Cw5u9roXgAAQLM0.jpg"
+   },
+   ,
+   {
+      "id": "66",
+      "description": "Deport back to Africa",
+      "imageURL":"https://pbs.twimg.com/media/Cw3gXV4WgAAJYl-.jpg"
+   },
+   {
+      "id": "67",
+      "description": "APU",
+      "imageURL":"https://dl2.pushbulletusercontent.com/pWEv9nmKE6UFGjqhmwPC34bfYeHuoJaf/Screen%20Shot%202016-11-12%20at%205.08.52%20PM.png"
+   },
+   {
+      "id": "68",
+      "description": "Nazi Flag",
+      "imageURL":"https://dl2.pushbulletusercontent.com/c8UH3rSrJ7F8rqma56DmNjt1wEY7v77p/Screen%20Shot%202016-11-12%20at%205.10.23%20PM.png"
+   },
+   {
+      "id": "69",
+      "description": "Old White man Threat",
+      "imageURL":"https://pbs.twimg.com/media/Cw3qSAgXgAEt4HM.jpg:large"
+   },
+   {
+      "id": "70",
+      "description": "Texas Flag",
+      "imageURL":"https://pbs.twimg.com/media/Cw7d-7xVQAEHCK-.jpg"
+   },
+   {
+      "id": "71",
+      "description": "White student racist said black student to get back in place",
+      "imageURL":"https://dl2.pushbulletusercontent.com/7Qyp8tdSGhr3Eu1tHwv50Lx0whCzdjQ4/Screen%20Shot%202016-11-12%20at%205.13.13%20PM.png"
+   },
+   {
+      "id": "72",
+      "description": "Tried to harass",
+      "imageURL":"https://pbs.twimg.com/media/Cw3rTc9WEAA8SJE.jpg:large"
+   },
+   {
+      "id": "73",
+      "description": "Go back to Africa, Trump doesn't want you here",
+      "imageURL":"https://pbs.twimg.com/media/Cw3pAtLWIAAt-bd.jpg"
+   },
+   {
+      "id": "74",
+      "description": "Tried to harass",
+      "imageURL":"https://pbs.twimg.com/media/Cw3rTc9WEAA8SJE.jpg:large"
+   },
+   {
+      "id": "75",
+      "description": "Go back to Africa",
+      "imageURL":"https://pbs.twimg.com/media/Cw3VEBPXgAEo_4F.jpg:large"
+   },
+   {
+      "id": "76",
+      "description": "Racist to immigrant gardner",
+      "imageURL":"https://pbs.twimg.com/media/Cw3Uaq7XEAAFnSJ.jpg:large"
+   }
+];
+
+
+/*React Code Below */
+var RepeatModule = React.createClass({
+   getInitialState: function() {
+      return { items: [] }
+   },
+   render: function() {
+      var listItems = this.props.items.map(function(item) {
+         return (
+            <div className='brick'>
+               <a target='_blank' href={item.productLink}><img src={item.imageURL}/></a>
+            </div>
+         );
+      });
+
+      return (
+         <div>
+            {listItems}
+         </div>
+      );
+   }
+});
+
+ReactDOM.render(<RepeatModule items={items} />,             
+   document.getElementById('hate-content'));
+
